291cd9c41586152a71807014d4d5da51b4aaecc5
Apollo Samuel <iamapollosamuel@gmail.com>
2020-08-29 22:24:29 -0300

    Add files via upload

---
+++ condicional_ifelse/exercicio3.java
@@ -0,0 +1,28 @@
+package condicional_ifelse;
+
+import java.util.Scanner;
+
+public class exercicio3 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		double salario = 240;
+		
+		System.out.println("Digite o total de vendas: ");
+		double vendas = input.nextDouble(); 
+		
+		if (vendas > 1000 && vendas < 10000) {
+			salario = (vendas*0.10) + salario;
+		} else if (vendas >= 10000) {
+			salario = salario + 1000;
+		} else {
+			salario = salario;
+		}
+		
+		System.out.println("Salário = " + salario);
+		
+		input.close();
+	}
+
+}
