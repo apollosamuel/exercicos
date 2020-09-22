221dfaf92471e45a4971dabaaf2948f3d97ff94f
Apollo Samuel <iamapollosamuel@gmail.com>
2020-09-22 08:24:05 -0300

    Add files via upload

---
+++ estrutura_repeticao_for/exercicio22.java
@@ -0,0 +1,18 @@
+package estrutura_repeticao_for;
+
+public class exercicio22 {
+
+	public static void main(String[] args) {
+		
+		int quantidade = 0;
+		double soma = 0;
+		for (int i = 1000; i < 10000; i++) {
+			if(i/1000 == i%10 && (i/100)%10 == (i/10)%10) {
+				soma+=i;
+				quantidade++;
+			}
+		}
+		System.out.println("Média: " + soma / quantidade);
+	}
+
+}
