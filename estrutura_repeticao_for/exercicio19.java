97546d37022db747c22771f9bf51df24340b6ed1
Apollo Samuel <iamapollosamuel@gmail.com>
2020-09-22 07:55:03 -0300

    Add files via upload

---
+++ estrutura_repeticao_for/exercicio19.java
@@ -0,0 +1,14 @@
+package estrutura_repeticao_for;
+
+public class exercicio19 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i <= 11; i++) {
+			System.out.println(Math.pow(2, i));
+			
+		}
+
+	}
+
+}
