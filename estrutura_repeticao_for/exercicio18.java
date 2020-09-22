56a6bffd8c510fb4c036100a68b1bc8c13d94db8
Apollo Samuel <iamapollosamuel@gmail.com>
2020-09-22 07:53:18 -0300

    Add files via upload

---
+++ estrutura_repeticao_for/exercicio18.java
@@ -0,0 +1,14 @@
+package estrutura_repeticao_for;
+
+public class exercicio18 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1; i <= 10; i++) {
+			System.out.println(i*i);
+			
+		}
+
+	}
+
+}
