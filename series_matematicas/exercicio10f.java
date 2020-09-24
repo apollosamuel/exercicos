0036312c6f12e8ad447a584efc6a5f968a95ca5d
Apollo Samuel <iamapollosamuel@gmail.com>
2020-09-23 23:48:14 -0300

    Add files via upload

---
+++ series_matematicas/exercicio10f.java
@@ -0,0 +1,24 @@
+package series_matematicas;
+
+public class exercicio10f {
+
+	public static void main(String[] args) {
+		
+		double serie = 0;
+		int num1 = 0;
+		
+		for(int i = 1; i <= 100; i++) {
+			
+
+			num1 += 2;
+			
+			
+			serie += Math.pow(i, 2)/(Math.pow(num2, 2));
+			
+		}
+		
+		System.out.println(serie);
+
+	}
+
+}
