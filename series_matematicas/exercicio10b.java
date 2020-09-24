a5095f2a018acf5adfdbfbaedc21092dec6d4fa1
Apollo Samuel <iamapollosamuel@gmail.com>
2020-09-23 22:38:40 -0300

    Add files via upload

---
+++ series_matematicas/exercicio10b.java
@@ -0,0 +1,24 @@
+package series_matematicas;
+
+public class exercicio10b {
+
+	public static void main(String[] args) {
+		
+		double serie = 0;
+		int num1 = -2; 
+		int num2 = 4;
+		
+		for(int i = 1; i <= 100; i++) {
+			
+			num1 += 3;
+			num2 += 3;
+			
+			serie += ((double)num1)/Math.sqrt(num2);
+
+		}
+
+		System.out.println((5.0/3.0)*serie);
+
+	}
+
+}
