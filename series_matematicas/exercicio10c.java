d0f071e6fff7a6e4b7d298f83c87b636acd68244
Apollo Samuel <iamapollosamuel@gmail.com>
2020-09-23 23:37:17 -0300

    Add files via upload

--- series_matematicas/exercicio10c.java
+++ series_matematicas/exercicio10c.java
@@ -5,20 +5,19 @@ public class exercicio10c {
 	public static void main(String[] args) {
 		
 		double serie = 0;
-		int num1 = 1;
-		int num2 = 1;
-		
+		int num1 = -1;
+		int num2 = 0;
 		
 		for(int i = 1; i <= 100; i++) {
 			
 			num1 += 2;
-			num2 += 4;
-
-			serie += ((double)num1*Math.sqrt(num2))/Math.pow(5,i);
-
+			num2 += 2;
+			
+			serie += ((double)(num1))/(num2);
+			
 		}
-
-		System.out.println(Math.pow(serie, 2));
+		
+		System.out.println((1.0/3)*(Math.pow(serie, 2)));
 
 	}
 
