9fb4ab8916f2900a5013a1ee5658623e39544182
Apollo Samuel <iamapollosamuel@gmail.com>
2020-09-23 23:45:43 -0300

    Add files via upload

--- series_matematicas/exercicio10e.java
+++ series_matematicas/exercicio10e.java
@@ -5,22 +5,17 @@ public class exercicio10e {
 	public static void main(String[] args) {
 		
 		double serie = 0;
-		int num1 = -2;
-		int num2 = 3;
-		int den1 = 0;
+		int num1 = 0;
 		
 		for(int i = 1; i <= 100; i++) {
 			
-			num1 += 3;
-			num2 += i*i;
-			den1 = i+1;
-			
-			
-			serie += ((double)num1*Math.sqrt(num2))/Math.pow(den1, i);
+			num1 += 2;
+	
+			serie += ((double)i)/(3*num1);
 			
 		}
 		
-		System.out.println((2.0/3.0)*serie);
+		System.out.println(serie);
 	}
 
 }
