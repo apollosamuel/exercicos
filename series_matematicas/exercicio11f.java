edad985684df09d0f17f7dc557b13d1aa606e518
Apollo Samuel <iamapollosamuel@gmail.com>
2020-09-23 23:24:35 -0300

    Add files via upload

--- series_matematicas/exercicio11f.java
+++ series_matematicas/exercicio11f.java
@@ -5,24 +5,15 @@ public class exercicio11f {
 	public static void main(String[] args) {
 		
 		double serie = 0;
-		int num1 = -1; 
-		int num2 = 2;
-		int num3 = 0;
-		int den = -3;
 		
-		for(int i = 1; i <= 2; i++) {
+		for(int i = 1; i <= 100; i++) {
 			
-			num1 += 2;
-			num3 = num2 + num2;
-			
-			den += 5;
-			
-			serie += (num1)/Math.pow(den, 2);
+			serie += Math.pow(2, i+1)*(2*i -1)/Math.pow(5*i -3, 2);
 			
 		}
 		
-		System.out.println(5*(Math.sqrt(serie)));
-		System.out.println(num2);
+		System.out.println(Math.sqrt(serie)*5);
+
 
 	}
 
