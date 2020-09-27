229ac8ad05b127134373b044ac4907277b88b47b
Apollo Samuel <iamapollosamuel@gmail.com>
2020-09-26 23:35:29 -0300

    Add files via upload

---
+++ while_rep/exercicio2.java
@@ -0,0 +1,15 @@
+package while_rep;
+
+import java.util.Scanner;
+
+public class exercicio2 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		
+		
+		input.close();
+	}
+
+}
