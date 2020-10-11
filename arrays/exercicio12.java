9ec4119f6e69188300bdfa7b3c99e1ce31985907
Apollo Samuel <iamapollosamuel@gmail.com>
2020-10-11 17:38:36 -0300

    Add files via upload

---
+++ arrays/exercicio12.java
@@ -0,0 +1,41 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class exercicio12 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+	
+
+			int nota[] = new int [3];
+			String nome[] = new String [3];
+			String nomeMaior[] = new String [3];
+			int notaMaior = 0;
+
+			for(int i = 0; i < nota.length; i++) {
+
+				System.out.println("Digite a nota");
+				nota[i] = input.nextInt(); input.nextLine(); 
+				System.out.println("Digite o nome");
+				nome[i] = input.nextLine(); 
+			}		
+
+
+			for(int i = 0; i < nota.length; i++) {
+				
+				if(nota[i] >= notaMaior) {
+					nomeMaior[i] = nome[i];
+					notaMaior = nota[i];
+					System.out.println("Maior nota: " + nomeMaior[i]);
+				}
+			}
+			
+
+			
+			input.close();
+		
+		input.close();
+	}
+
+}
